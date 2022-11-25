2e21a4c129824195bc42b0dfe38e4c9400c2f0db
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-11-25 15:10:22 +0100

    Add files via upload

--- superbenouvelessai.py
+++ superbenouvelessai.py
@@ -4,15 +4,46 @@ Created on Fri Nov 25 14:44:54 2022
 
 @author: nguil
 """
+
 import streamlit as st
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
 
 st.title('Hello Wilders, welcome to my application!')
 
-st.write("I enjoy to discover stremalit possibilities")
+st.write("I enjoy to discover streamlit possibilities")
 
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
 df_weather = pd.read_csv(link)
 
 # Here we use "magic commands":
 df_weather
+
+data = pd.DataFrame({
+    'awesome cities' : ['Chicago', 'Minneapolis', 'Louisville', 'Topeka'],
+    'lat' : [41.868171, 44.979840,  38.257972, 39.030575],
+    'lon' : [-87.667458, -93.272474, -85.765187,  -95.702548]
+})
+
+# Adding code so we can have map default to the center of the data
+midpoint = (np.average(data['lat']), np.average(data['lon']))
+
+st.deck_gl_chart(
+            viewport={
+                'latitude': midpoint[0],
+                'longitude':  midpoint[1],
+                'zoom': 4
+            },
+            layers=[{
+                'type': 'ScatterplotLayer',
+                'data': data,
+                'radiusScale': 250,
+   'radiusMinPixels': 5,
+                'getFillColor': [248, 24, 148],
+            }]
+        )
+

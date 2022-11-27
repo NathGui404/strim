c463157a405ad0e3d28f46042157b190c252522a
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-11-27 13:50:22 +0100

    Update superbenouvelessai.py

--- superbenouvelessai.py
+++ superbenouvelessai.py
@@ -29,18 +29,5 @@ data = pd.DataFrame({
 # Adding code so we can have map default to the center of the data
 midpoint = (np.average(data['lat']), np.average(data['lon']))
 
-st.deck_gl_chart(
-            viewport={
-                'latitude': midpoint[0],
-                'longitude':  midpoint[1],
-                'zoom': 4
-            },
-            layers=[{
-                'type': 'ScatterplotLayer',
-                'data': data,
-                'radiusScale': 250,
-   'radiusMinPixels': 5,
-                'getFillColor': [248, 24, 148],
-            }]
-        )
+
 

442a6cf5c17610b554939295132d4bb11cf18eb4
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-11-27 16:42:24 +0100

    Add files via upload

---
+++ streamtest1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 27 15:31:18 2022
+
+@author: nguil
+"""
+
+import streamlit as st
+import pandas as pd
+
+st.title('Hello Wilders, welcome to my application!')
+
+st.write("I enjoy to discover stremalit possibilities")
+
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
+df_weather = pd.read_csv(link)
+st.write(df_weather)
+
+st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+
+import seaborn as sns
+viz_correlation = sns.heatmap(df_weather.corr(), 
+								center=0,
+								cmap = sns.color_palette("vlag", as_cmap=True)
+								)
+
+st.pyplot(viz_correlation.figure)
